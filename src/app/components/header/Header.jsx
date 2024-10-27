fcfdf4198012ef88f7731f63e6dc71a192c22bd7
CoyyCo <1805283656@qq.com>
2024-10-28 00:58:24 +0800

    Header Conponent

--- src/app/components/header/Header.jsx
+++ src/app/components/header/Header.jsx
@@ -1,20 +1,50 @@
+import Link from "next/link";
 import styles from "./Header.module.scss";
-
+import { useEffect, useState } from "react";
+import Head from "next/head";
 export default function Header() {
+  const [isScrolled, setIsScrolled] = useState(false);
+  useEffect(() => {
+    const handle = () => {
+      const y = window.scrollY;
+      if (y > 56) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+    window.addEventListener("scroll", handle);
+    return () => window.removeEventListener("scroll", handle);
+  }, []);
+  const gohref = () => {
+    alert('aa')
+  };
   return (
-    <header className={styles.header}>
-      <div className={`${styles.container} ${styles.flow}`}>
-        <img src="/images/logo.png" alt="logo" />
-        <nav>
-          <ul>
-            <li>Create</li>
-            <li>Export</li>
-            <li>Pricing</li>
-            <li>FQA</li>
-          </ul>
-        </nav>
-        <button>Try for free</button>
-      </div>
-    </header>
+    <>
+      <header
+        className={`${styles.header} ${isScrolled ? styles.scrolled : ""}`}
+      >
+        <div className={styles.container}>
+          <img src="/images/logo.png" alt="logo" />
+          <nav className={styles.nav1}>
+            <ul>
+              <li>
+                <Link href={"/"}>Create</Link>
+              </li>
+              <li>
+                <Link href={"/"}>Export</Link>
+              </li>
+              <li>
+                <Link href={"/"}>Pricing</Link>
+              </li>
+              <li>
+                <Link href={"/"}>FQA</Link>
+              </li>
+            </ul>
+          </nav>
+          <button onClick={gohref}>Try for free</button>
+        </div>
+      </header>
+    </>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,34 @@
+"use client";
 import styles from "./page.module.css";
-import Header from "./components/header/Header"
+import Header from "./components/header/Header";
+import Head from "next/head";
+import { useEffect } from "react";
 export default function Home() {
+  useEffect(() => {
+    const handleResize = () => {
+      document.documentElement.style.setProperty(
+        "--scaleXrate",
+        `${Math.ceil(window.innerWidth / 300)}`
+      );
+    };
+
+    handleResize(); // 初始化设置
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
   return (
     <>
       <Header></Header>
+      <div
+        style={{
+          height: "110vh",
+          width: "100%",
+          background: "rgb(255,0,0,0.3)",
+        }}
+      ></div>
     </>
   );
 }
